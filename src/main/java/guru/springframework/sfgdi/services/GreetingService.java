d06ca07ad9b64b354b77703ec210a426f3ad57c4
bpk81 <pierrick.brunaud@keonys.com>
2023-10-19 10:01:12 +0200

    second commit

---
+++ src/main/java/guru/springframework/sfgdi/services/GreetingService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+
+public interface GreetingService {
+    String sayGreeting();
+}
